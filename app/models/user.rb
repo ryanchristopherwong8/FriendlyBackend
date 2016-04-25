aa3a84303524f1f55854034fea31061a2165c20b
Ryan Wong <ryanwong@Ryans-MacBook-Pro.local>
2016-04-25 02:27:58 -0700

    Adds authentication token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  #before creating a user must generate authentication token
+  before_create :generate_authentication_token!
+
+  #flow for authenticating users through an API
+  #1.Client requests for sessions resources with email and password
+  #2.Server returns user resource along with authentication token
+  #3.Pages that require authentication, client must send authentication token
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424070519) do
+ActiveRecord::Schema.define(version: 20160425091657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 20160424070519) do
     t.datetime "updated_at",                                                   null: false
     t.decimal  "longitude",              precision: 10, scale: 6
     t.decimal  "latitude",               precision: 10, scale: 6
+    t.string   "auth_token",                                      default: ""
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
